24f6c9ba409e3221202c8fdf3bbfe9f063c7e964
simo1106 <hohotyt999888@gmail.com>
2026-04-28 16:20:21 +0800

    2026-4-28 FloatField 更改符合 溫度欄位

--- myapp/models.py
+++ myapp/models.py
@@ -8,7 +8,7 @@ class Temperature(models.Model):
     #timestamp datatime
     myid = models.AutoField(primary_key=True)
     sensor_id = models.IntegerField(blank=False, null=False)
-    temperature= models.IntegerField(blank=False, null=False)
+    temperature= models.FloatField(blank=False, null=False)
     humidity = models.FloatField(blank=False, null=False)
     timestamp = models.DateTimeField(auto_now=True)
     def __str__(self):
