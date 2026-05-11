7dd4a6074bb6c8ae334a19814c9c6df76b27f8a7
simo1106 <hohotyt999888@gmail.com>
2026-05-11 11:40:20 +0800

    2026-5-11 - 更改名字folder

--- Django-Temperature-Manager/urls.py
+++ Django-Temperature-Manager/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for homework6 project.
+URL configuration for Django-Temperature-Manager project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
